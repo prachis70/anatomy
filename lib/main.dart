5aa807756eaf08bbde2bb476ca55faee427055cf
prachi_070 <149580593+prachis70@users.noreply.github.com>
2024-03-20 15:15:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,36 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+//hello world
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home:Scaffold(
+      home: SafeArea(
+          child: Scaffold(
+        //appBar: (),
         backgroundColor: Colors.black,
-        body: Center(
-          child:Text('hello \n\n Dart \n\n Fultter',style:TextStyle(fontSize: 50,fontWeight:FontWeight.bold,color: Colors.red,decoration: TextDecoration.underline,decorationColor: Colors.amber, ), ),
-
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: (Center(
+            child: Text(
+          "Hello\n\n\n\nDart\n\n\n\nFlutter",
+          style: TextStyle(
+              color: Colors.redAccent,
+              fontSize: 45,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.double,
+              decorationColor: Colors.yellowAccent),
+        ))),
+      )),
     );
   }
 }
